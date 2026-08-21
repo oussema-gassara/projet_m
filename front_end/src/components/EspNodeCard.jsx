7c9b1b82637123e3f6ba63647dd25437d954cef2
oussema-gassara <oussama.ghassar@issatm.ucar.tn>
2026-08-21 16:21:15 +0100

    Add remove button to ESP32 cards

--- front_end/src/components/EspNodeCard.jsx
+++ front_end/src/components/EspNodeCard.jsx
@@ -2,10 +2,24 @@ import SystemControl from "./systemControl.jsx";
 import SensorControl from "./sensorControl.jsx";
 import NetworkControl from "./networkControl.jsx";
 
-export default function EspNodeCard({ nodeName, testMode = false }) {
+export default function EspNodeCard({ node, testMode = false, onRemove }) {
+    const nodeName = node.node_name;
+
     return (
         <div className="esp-node-card">
-            <h2 className="esp-node-title">{nodeName}</h2>
+            <div className="esp-node-header">
+                <h2 className="esp-node-title">{nodeName}</h2>
+
+                {onRemove && (
+                    <button
+                        className="remove-node-button"
+                        onClick={() => onRemove(node)}
+                        type="button"
+                    >
+                        Supprimer
+                    </button>
+                )}
+            </div>
 
             <div className="main">
                 <SystemControl nodeName={nodeName} testMode={testMode} />
